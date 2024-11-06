2a54f09eef5ee3d61f099ca7d7476c1c9f3bc684
maulanamb <mmbhaswara@gmail.com>
2024-11-07 00:48:29 +0700

    Add files via upload

---
+++ JavaScript Function/nomor4.js
@@ -0,0 +1,11 @@
+const sayHello = name => `Hello ${name}! How are you?`;
+
+console.log(sayHello("Vincent"));
+console.log(sayHello("Hagrid"));
+console.log(sayHello("Abe"));
+
+
+const sayHi = nama => `Hello ${nama}! How are you?`;
+
+console.log(sayHi("Maulana"));
+console.log(sayHi("Bhaswara"));
