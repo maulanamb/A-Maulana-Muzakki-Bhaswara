9701a8a9ab0700ecc8291c2aaf6bb96abfb7379b
maulanamb <mmbhaswara@gmail.com>
2024-11-07 13:32:52 +0700

    Add files via upload

---
+++ JavaScript String/belajar.js
@@ -0,0 +1,36 @@
+const antrian = ["Ray", "Fiki", "Fadhilla", "Farah"];
+console.log(antrian);
+
+antrian.push("Nabila")
+console.log(antrian);
+
+antrian.push("Maza", "Elsi");
+console.log(antrian);
+
+antrian.pop();
+console.log(antrian);
+
+antrian.shift();
+console.log(antrian);
+
+antrian.shift("Fiki");
+console.log(antrian);
+
+antrian.unshift("Thomi");
+console.log(antrian);
+
+
+// ada sebuah antrian pembeli yaitu ray, fiki, fadhilla, farah
+
+//datang 1 pembeli yaitu nabila
+//datang 2 pembeli yaitu maza dan elsi
+
+//antrian terakhir tidak jadi antri dan pulang ke rumah
+
+//antrian pertama sudah mendapatkan belanjaannya
+
+//antrian kedua juga sudah mendapatkan belanjaannya
+
+//datang pembeli baru yang nyerobot antrian yaitu tomi
+
+//maka hasil akhirnya yaitu ...
